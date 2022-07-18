1a01029f329af7397dc40a42345aba28adf982b7
Thomas Wessel <turpleturtle12@gmail.com>
2022-07-18 00:27:27 -0700

    Added Creative Tab
    added creative tab with a new en_us lang file

--- src/main/java/com/tcn/superultraepicmod/SuperUltraEpicMod.java
+++ src/main/java/com/tcn/superultraepicmod/SuperUltraEpicMod.java
@@ -1,6 +1,11 @@
 package com.tcn.superultraepicmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Blocks;
 
 public class SuperUltraEpicMod implements ModInitializer
 {
@@ -8,4 +13,9 @@ public class SuperUltraEpicMod implements ModInitializer
     public void onInitialize() {
 
     }
+
+    public static final CreativeModeTab CREATIVE_TAB = FabricItemGroupBuilder.build(
+            new ResourceLocation("super-ultra-epic-mod", "creative_tab"),
+            () -> new ItemStack(Blocks.TURTLE_EGG) //TODO: find a better block for this
+    );
 }
