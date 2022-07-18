0b5ab37189e2a065009307185369382efd21f706
Jake Kallos <yourface255@yahoo.com>
2022-07-18 15:06:05 -0700

    e

--- src/main/java/com/tcn/superultraepicmod/SuperUltraEpicMod.java
+++ src/main/java/com/tcn/superultraepicmod/SuperUltraEpicMod.java
@@ -5,13 +5,17 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.vehicle.Boat;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.material.Material;
 
 public class SuperUltraEpicMod implements ModInitializer
 {
@@ -20,6 +24,9 @@ public class SuperUltraEpicMod implements ModInitializer
     public void onInitialize() {
         Registry.register(Registry.ITEM, new ResourceLocation(MOD_ID, "crimson_boat"), CRIMSON_BOAT);
         Registry.register(Registry.ITEM, new ResourceLocation(MOD_ID, "warped_boat"), WARPED_BOAT);
+        Registry.register(Registry.ITEM, new ResourceLocation(MOD_ID, "flesh_block_drop"), new BlockItem(ROTTENFLESH_BLOCK, new FabricItemSettings().group(CREATIVE_TAB)));
+        //start block registry
+        Registry.register(Registry.BLOCK, new ResourceLocation(MOD_ID, "flesh_block"), ROTTENFLESH_BLOCK);
     }
 
     //Creative tabs
@@ -33,4 +40,19 @@ public class SuperUltraEpicMod implements ModInitializer
     public static final Item WARPED_BOAT = new NetherBoatItem(false, Boat.Type.JUNGLE, new FabricItemSettings().group(CREATIVE_TAB).fireResistant());
 
     //block instantiation
+
+
+        /* Declare and initialize our custom block instance.
+           We set our block material to `METAL`, which requires a pickaxe to efficiently break.
+
+           `strength` sets both the hardness and the resistance of a block to the same value.
+           Hardness determines how long the block takes to break, and resistance determines how strong the block is against blast damage (e.g. explosions).
+           Stone has a hardness of 1.5f and a resistance of 6.0f, while Obsidian has a hardness of 50.0f and a resistance of 1200.0f.
+
+           You can find the stats of all vanilla blocks in the class `Blocks`, where you can also reference other blocks.
+        */
+        public static final Block ROTTENFLESH_BLOCK = new Block(FabricBlockSettings.of(Material.WEB).strength(0.6f));
+
+
+
 }
---
+++ src/main/java/com/tcn/superultraepicmod/blocks/fleshblock.java
@@ -0,0 +1,11 @@
+package com.tcn.superultraepicmod.blocks;
+
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.level.block.Block;
+
+public class fleshblock extends Block {
+    public fleshblock(Properties properties) {
+        super(properties);
+    }
+
+}
