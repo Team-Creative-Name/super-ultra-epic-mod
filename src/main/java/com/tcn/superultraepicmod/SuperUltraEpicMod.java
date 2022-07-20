aed71b20fe070c9b982a21f03edec6f411d4e9ba
Jake Kallos <yourface255@yahoo.com>
2022-07-20 11:17:40 -0700

    removed an incorrect comment

--- src/main/java/com/tcn/superultraepicmod/SuperUltraEpicMod.java
+++ src/main/java/com/tcn/superultraepicmod/SuperUltraEpicMod.java
@@ -41,17 +41,7 @@ public class SuperUltraEpicMod implements ModInitializer
     public static final Item WARPED_BOAT = new NetherBoatItem(false, Boat.Type.JUNGLE, new FabricItemSettings().group(CREATIVE_TAB).fireResistant());
 
     //block instantiation
-
-
-        /* Declare and initialize our custom block instance.
-           We set our block material to `METAL`, which requires a pickaxe to efficiently break.
-
-           `strength` sets both the hardness and the resistance of a block to the same value.
-           Hardness determines how long the block takes to break, and resistance determines how strong the block is against blast damage (e.g. explosions).
-           Stone has a hardness of 1.5f and a resistance of 6.0f, while Obsidian has a hardness of 50.0f and a resistance of 1200.0f.
-
-           You can find the stats of all vanilla blocks in the class `Blocks`, where you can also reference other blocks.
-        */
+    
         public static final Block ROTTEN_FLESH_BLOCK = new Block(FabricBlockSettings.of(Material.WOOD).strength(0.6f).sound(SoundType.SLIME_BLOCK));
 
 
